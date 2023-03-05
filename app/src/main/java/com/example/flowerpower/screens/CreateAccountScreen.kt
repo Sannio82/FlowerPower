aad969938de01d69edc20ded379b40eb67a3b81c
Sannio82 <90681257+Sannio82@users.noreply.github.com>
2023-03-05 12:20:30 +0100

    Added screens, navigations and layout (#2)
    * Change gradle
    
    * Add screens and viewmodel
    
    * Add navigation to Login
    
    * Add navigation to Create account
    
    * Change create account screen background

---
+++ app/src/main/java/com/example/flowerpower/screens/CreateAccountScreen.kt
@@ -0,0 +1,31 @@
+package com.example.flowerpower.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.navigation.NavController
+import com.example.flowerpower.ui.theme.Coral
+import com.example.flowerpower.ui.theme.Yellow
+
+@Composable
+fun CreateAccountScreen(navController: NavController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Coral,
+                        Yellow
+                    )
+                )
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        "Hej hej allihopa!"
+    }
+}
---
+++ app/src/main/java/com/example/flowerpower/viewmodel/WelcomeScreenContract.kt
@@ -0,0 +1,5 @@
+package com.example.flowerpower.viewmodel
+
+sealed interface WelcomeScreenActions {
+    object ButtonClicked : WelcomeScreenActions
+}
