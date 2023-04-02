fee76d8105ef513666e67d87709cabf6f797b93c
sannio82 <sannajojo82@gmail.com>
2023-04-02 20:41:38 +0200

    Can create account and login

--- app/src/main/java/com/example/flowerpower/screens/CreateAccountScreen.kt
+++ app/src/main/java/com/example/flowerpower/screens/CreateAccountScreen.kt
@@ -1,20 +1,78 @@
 package com.example.flowerpower.screens
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.flowerpower.ui.theme.Coral
 import com.example.flowerpower.ui.theme.Yellow
+import com.example.flowerpower.viewmodels.AuthViewModel
+import com.example.flowerpower.viewmodels.UserLoginStatus
+import com.example.flowerpower.views.buttons.composables.FlowerPowerField
+
 
 @Composable
-fun CreateAccountScreen(navController: NavController) {
+fun CreateAccountScreen(navController: NavController)
+{
+    val viewModel: AuthViewModel = viewModel()
+
+    val localContext = LocalContext.current
+
+    var userName by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    val loginStatus by viewModel.userLoginStatus.collectAsState()
+
+    var showFailedDialog by remember {
+        mutableStateOf(false)
+    }
+
+    LaunchedEffect(key1 = loginStatus ) {
+        when(loginStatus) {
+            is UserLoginStatus.Failure -> {
+                localContext.showToast("Unable to login")
+                println("Can not login!!!")
+                showFailedDialog = true
+            }
+            UserLoginStatus.Successful -> {
+                localContext.showToast("Login successful")
+                navController.navigate("PlantsScreen")
+            }
+            null -> {
+
+            }
+        }
+    }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -26,10 +84,124 @@ fun CreateAccountScreen(navController: NavController) {
                     )
                 )
             ),
-        contentAlignment = Alignment.Center
+        contentAlignment = Alignment.Center)
+    {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(28.dp)
+                .alpha(0.6f)
+                .clip(
+                    CutCornerShape(
+                        topStart = 8.dp,
+                        topEnd = 16.dp,
+                        bottomStart = 16.dp,
+                        bottomEnd = 8.dp
+                    )
+                )
+                .background(Color.White)
+        )
+
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(48.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            SignUpHeader()
+            SignUpFields(userName, password, navController = navController,
+                onUsernameChange = {
+                    userName = it
+                },
+                onPasswordChange = {
+                    password = it
+                },
+            )
+            SignUpFooter(
+                onSignUpClick = {
+                    when {
+                        userName.isBlank() -> {
+                            //Use error field for this
+                            localContext.showToast("Enter your username")
+                        }
+                        password.isBlank() -> {
+                            localContext.showToast("Enter your password")
+                        } else -> {
+                        viewModel.createAccount(userName,password)
+                    }
+                    }
+                }
+            )
+        }
+    }
+
+    if(showFailedDialog) {
+        //Alert Dialog
+    }
+}
+
+@Composable
+fun SignUpHeader() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(text = "CreateAccount!", fontSize = 36.sp, fontWeight = FontWeight.ExtraBold)
+    }
+}
+
+@Composable
+fun SignUpFields(username: String, password: String,
+                onUsernameChange: (String) -> Unit,
+                onPasswordChange: (String) -> Unit,
+                navController: NavController
+) {
+    Column() {
+        FlowerPowerField(
+            value = username,
+            label = "Username",
+            placeholder = "Enter your email address",
+            onValueChange = onUsernameChange,
+            leadingIcon = {
+                Icon(Icons.Default.Email, contentDescription = "Email")
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email, imeAction = ImeAction.Next)
+        )
+        Spacer(Modifier.height(8.dp))
+
+        FlowerPowerField(value = password,
+            label = "Password",
+            placeholder = "Enter your password",
+            onValueChange = onPasswordChange,
+            visualTransformation = PasswordVisualTransformation(),
+            leadingIcon = {
+                Icon(Icons.Default.Lock, contentDescription = "Password")
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password, imeAction = ImeAction.Go)
+        )
+
         Button(onClick = { navController.navigate("WelcomeScreen")}) {
             Text("Tillbaka!")
         }
     }
 }
+
+@Composable
+fun SignUpFooter(
+    onSignUpClick: () -> Unit
+) {
+    Column() {
+        Button(onClick = onSignUpClick, modifier = Modifier.fillMaxWidth()) {
+            Text("Sign up")
+        }
+    }
+}
+
+fun Context.showSignUpToast(msg: String) {
+    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+}
+
+
+
+
+
