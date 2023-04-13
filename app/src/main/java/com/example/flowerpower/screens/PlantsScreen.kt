a01b0a63e466279ea79c08c415a204d5db329622
sannio82 <sannajojo82@gmail.com>
2023-04-13 20:32:43 +0200

    Create PlantCard and List

--- app/src/main/java/com/example/flowerpower/screens/PlantsScreen.kt
+++ app/src/main/java/com/example/flowerpower/screens/PlantsScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,6 +16,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.flowerpower.ui.theme.Coral
 import com.example.flowerpower.ui.theme.Yellow
+import com.example.flowerpower.viewmodels.Plant
+import com.example.flowerpower.viewmodels.plantList
+import com.example.flowerpower.views.composables.PlantCard
 
 @Composable
 fun PlantsScreen() {
@@ -34,12 +39,18 @@ fun PlantsScreen() {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(top = 20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-        Text(text = "Mina plantor!",
-            fontSize = 35.sp
+            Text(text = "Mina plantor!",
+                fontSize = 35.sp
             )
-
+            LazyColumn(
+                modifier = Modifier.padding(bottom = 35.dp)
+            ) {
+                items(plantList) {plant ->
+                    PlantCard(plant = plant)
+                }
+            }
         }
     }
 }
