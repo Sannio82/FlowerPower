347f08c3a7ebade804ac041e89063a7b05262d3d
sannio82 <sannajojo82@gmail.com>
2023-04-13 20:02:41 +0200

    Fixup on plantScreen

--- app/src/main/java/com/example/flowerpower/screens/PlantsScreen.kt
+++ app/src/main/java/com/example/flowerpower/screens/PlantsScreen.kt
@@ -2,12 +2,15 @@ package com.example.flowerpower.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.flowerpower.ui.theme.Coral
@@ -28,8 +31,16 @@ fun PlantsScreen() {
             ),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "Välkommen till plantskärmen!",
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Text(text = "Mina plantor!",
             fontSize = 35.sp
             )
+
+        }
     }
 }
