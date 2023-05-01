524f7f112a9e3fddfdbbbea299b621e693650454
Sanna Johansson <susanna.johansson@iths.se>
2023-05-01 16:51:32 +0200

    Add button close

--- app/src/main/java/com/example/flowerpower/views/CreateNewPlantView.kt
+++ app/src/main/java/com/example/flowerpower/views/CreateNewPlantView.kt
@@ -3,12 +3,15 @@ package com.example.flowerpower.views
 import android.text.TextUtils
 import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
@@ -46,9 +49,26 @@ fun CreateNewPlantView() {
                 .fillMaxSize(0.92f)
                 .clip(shape = RoundedCornerShape(15.dp))
                 .background(Beige)
-                .padding(15.dp),
+                .padding(top = 20.dp, start = 20.dp, end = 20.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.TopEnd
+            ) {
+                IconButton(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .border(width = 2.dp, color = Blue, shape = CircleShape)
+                ) {
+                    Icon(
+                        Icons.Default.Close,
+                        contentDescription = "Add image"
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.size(30.dp))
             Text(
                 text = "Lägg till ny planta",
                 fontSize = 35.sp,
@@ -84,7 +104,9 @@ fun CreateNewPlantView() {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 40.dp, bottom = 20.dp, start = 20.dp, end = 20.dp)
+                    .padding(top = 40.dp, bottom = 20.dp, start = 20.dp, end = 20.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
             ) {
                 GradientButton(
                     text = "Spara",
